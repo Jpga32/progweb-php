db5980d5bb2401cbe280bd2116315b501e94d136
João Pedro <pedroganacim@gmail.com>
2022-04-28 14:42:25 -0300

    reformulando exercicio 003

--- exercicios-php/003-exercicio-condicionais.php
+++ exercicios-php/003-exercicio-condicionais.php
@@ -7,12 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio #003 Condicionais</title>
 </head>
+<style>
+    .antigo {color:orangered;font-size: 18px;font-weight: bold;}
+    .novo {color:blue;font-size: 18px;font-weight: bold;}
+    .texto { font-size: 18px;font-weight:700;}
+</style>
 
 <body>
     <h1>Exercicio 03 Usando Condicionais</h1>
     <hr>
+    <h2>Exebindo resultado dentro do PHP</h2>
     <?php
-    $salario = 600;
+    $salario = 500;
     
 
     if ($salario < 500) {
@@ -27,10 +33,15 @@
         $sal_reaj = $salario * 1.05;
     }
     
-    echo "<p> O seu salario antigo era de $salario e foi reajustado para $sal_reaj</p>";
+    echo "<p class='texto'> O seu salario antigo era de <span class='antigo'> R$$salario </span> e foi reajustado para<span class='novo'> R$$sal_reaj </span> </p>";
     ?>
+<hr>
+    <h2>Exebindo resultado fora do PHP</h2>
 
-    
+
+<p class='texto'> O seu salario antigo era de <span class='antigo'> R$ <?=number_format($salario,2,",",".")?> </span> e foi reajustado para <span class='novo'> R$<?=number_format($sal_reaj,2,",",".")?> </span> </p>
+
+
 
 </body>
 
